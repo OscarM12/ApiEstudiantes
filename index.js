0835863268b5387d6ced05d7056423c18c76f505
OscarM12 <L19100220@nlaredo.tecnm.mx>
2024-11-29 20:52:39 -0600

    index.js

--- index.js
+++ index.js
@@ -1,19 +1,15 @@
-import express from 'express';
+import express from "express";
 import bodyParser from "body-parser";
 import cors from "cors";
 import swaggerUI from "swagger-ui-express";
-import studentRouter from "./students/studensts.router.js";
+import studentRouter from "./students/students.router.js";
 import swaggerJSDoc from "swagger-jsdoc";
 import { SwaggerTheme } from "swagger-themes";
-import Redoc from 'redoc-express';
-import { testConnection } from './connection.js';  // Importar la función de prueba
-
-import { DB, DB_USER, DB_PASSWORD, HOST, SERVER_PORT } from './config.js'; // Importar la configuración
+import Redoc from 'redoc-express'
 
 const app = express();
 const port = process.env.PORT || 8080;
-const theme = new SwaggerTheme();
-const customCss = theme.getBuffer('dark'); 
+const theme = new SwaggerTheme('v3')
 
 const swaggerOptions = {
   definition: {
@@ -28,34 +24,34 @@ const swaggerOptions = {
       { name: "Estudiantes", description: "Operaciones relacionadas con estudiantes" },
     ],
   },
-  apis: ["./students/studensts.router.js"],
+  apis: ["./students/students.router.js"],
 };
+
 
 const options = {
   explorer: true,
-  customCss
+  customCss: theme.getBuffer('dark')
 };
-
-testConnection();  // Llamar a la función de prueba de conexión
 
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-
 const swaggerDocs = swaggerJSDoc(swaggerOptions);
-app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerDocs, options));
+app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerDocs,options));
 app.use(studentRouter);
 
-app.use('/api-docs-json', (req, res) => {
+app.use('/api-docs-json', (req,res) => {
   res.json(swaggerDocs);
-});
+})
 
-app.use('/redoc', Redoc({
+app.use('/docs', Redoc({
   title: 'Documentación de Mi API',
-  specUrl: '/api-docs-json',
+  specUrl: '/api-docs-json', 
 }));
 
-// Usar el puerto definido en config.js
-app.listen(SERVER_PORT, () => {
-  console.log(`Servidor corriendo en el puerto ${SERVER_PORT}`);
+
+app.listen(port, () => {
+  console.log(
+    `Servidor express escuchando en el puerto: http://localhost:${port}`
+  );
 });
