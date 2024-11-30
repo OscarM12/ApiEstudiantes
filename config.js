f7a182e209b6f921aa9d06d7e1675987995342bc
OscarM12 <L19100220@nlaredo.tecnm.mx>
2024-11-30 13:17:02 -0600

    Contrasenas

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 export const PORT = process.env.PORT || 8080;  // Puerto del servidor de la API
-export const HOST = process.env.HOST || 'localhost';  // Host de la base de datos
+export const HOST = process.env.HOST || 'junction.proxy.rlwy.net';  // Host de la base de datos
 export const DB = process.env.DB || 'estudiante';  // Nombre de la base de datos
 export const DB_USER = process.env.DB_USER || 'root';  // Usuario de la base de datos
-export const DB_PASSWORD = process.env.DB_PASSWORD || 'Morenoram12';  // Contraseña de la base de datos
-export const DB_PORT = process.env.DB_PORT || 3306;
+export const DB_PASSWORD = process.env.DB_PASSWORD || 'lakDCfAdVJabwilxJXpIAXsFSinNxLJe';  // Contraseña de la base de datos
+export const DB_PORT = process.env.DB_PORT || 55824;
   // Puerto de la base de datos
 
   import dotenv from 'dotenv';
