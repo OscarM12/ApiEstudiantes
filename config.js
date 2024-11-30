ef35e6d4cf2124db31a4523ae991682d1a2b51fb
OscarM12 <L19100220@nlaredo.tecnm.mx>
2024-11-29 20:50:33 -0600

    Config

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
-export const DB = 'estudiante';
-export const DB_USER = 'root';
-export const DB_PASSWORD = 'Morenoram12';
-export const HOST = 'localhost';
-export const SERVER_PORT = 3306;
+export const PORT = process.env.PORT || 3306;
+export const HOST = process.env.HOST || 'localhost';
+export const DB = process.env.DB || 'estudiante';
+export const DB_USER = process.env.DB_USER || 'root';
+export const DB_PASSWORD = process.env.DB_PASSWORD || 'Morenoram12';
