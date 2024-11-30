4e1638866ff240f3e46a63b5e131767bb05e90d4
OscarM12 <L19100220@nlaredo.tecnm.mx>
2024-11-29 20:17:46 -0600

    Modificaciones

--- connection.js
+++ connection.js
@@ -1,12 +1,14 @@
 import mysql from 'mysql2';
-import { DB,DB_PASSWORD,DB_USER,PORT,HOST } from './config.js';
+import config from './config.js'; // Importa el objeto config
+
+const { DB, DB_PASSWORD, DB_USER, DB_PORT, HOST } = config; // Desestructurar las variables de config
 
 export const pool = mysql.createPool({
   host: HOST,
   user: DB_USER,
   password: DB_PASSWORD,
   database: DB,
-  port: DB_PORT,
+  port: DB_PORT, // Asegúrate de que estás usando DB_PORT en lugar de PORT
   waitForConnections: true,
   connectionLimit: 10,
   maxIdle: 10,
@@ -15,6 +17,5 @@ export const pool = mysql.createPool({
   enableKeepAlive: true,
   keepAliveInitialDelay: 0,
 });
-
 
 export const promisePool = pool.promise();
