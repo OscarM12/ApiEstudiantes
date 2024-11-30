73591096ecf9a2738e29ac6fcbe065a2512f2444
OscarM12 <L19100220@nlaredo.tecnm.mx>
2024-11-29 20:04:16 -0600

    Config

--- config.js
+++ config.js
@@ -1,6 +1,5 @@
-export const DB_PORT = process.env.DB_PORT || 3306; 
-export const SERVER_PORT = process.env.PORT || 3000; 
-export const HOST = process.env.HOST || 'localhost';
-export const DB = process.env.DB || 'estudiante';
-export const DB_USER = process.env.DB_USER || 'root';
-export const DB_PASSWORD = process.env.DB_PASSWORD || 'Morenoram12';
+export const DB = 'estudiante';
+export const DB_PASSWORD = 'Morenoram12';
+export const DB_USER = 'root';
+export const HOST = 'localhost';
+export const PORT = 8080; // Asegúrate de que esta constante esté definida y exportada
