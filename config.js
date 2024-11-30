f312331974986ec007a24df9bfda9c648bfeda14
OscarM12 <L19100220@nlaredo.tecnm.mx>
2024-11-29 20:41:54 -0600

    Modificacione Puerto

--- config.js
+++ config.js
@@ -2,4 +2,4 @@ export const DB = 'estudiante';
 export const DB_USER = 'root';
 export const DB_PASSWORD = 'Morenoram12';
 export const HOST = 'localhost';
-export const SERVER_PORT = 8080;
+export const SERVER_PORT = 3306;
